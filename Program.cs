78b9dfcc660e85931eaafe5bd6cdcea2055fcdef
J4N3i <154697254+J4N3i@users.noreply.github.com>
2026-02-19 16:39:50 +0530

    Fix: Add missing app.Run() to keep app running

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using MyMicroservice.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// --- 1. Add Swagger Services ---
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// -------------------------------
+
+// Register the Database Context with SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// --- 2. Configure the HTTP request pipeline. ---{
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// -------
+app.MapControllers(); // <--- THIS WAS MISSING!
+
+// --- AUTO-CREATE DATABASE ON STARTUP ---
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // This creates the app.db file if it's missing
+    db.Database.EnsureCreated();
+}
+// ---------------------------------------
+app.Run();
